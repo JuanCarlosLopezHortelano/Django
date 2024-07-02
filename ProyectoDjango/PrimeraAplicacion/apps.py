5ad2e32307a51c89e9828a60067221c27eec0110
JuanCarlosLopezHortelano <juancarloslhhellin@gmail.com>
2024-07-02 16:31:52 +0200

    PrimeraAplicacionDjango

---
+++ ProyectoDjango/PrimeraAplicacion/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PrimeraaplicacionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'PrimeraAplicacion'
--- ProyectoDjango/ProyectoDjango/urls.py
+++ ProyectoDjango/ProyectoDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from PrimeraAplicacion.views import my_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('car-list/',my_view),
+
 ]
